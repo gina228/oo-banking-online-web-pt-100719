fbb3430781145118ea58231f555d874b8cdba01c
Gina Graci <ginagraci92@gmail.com>
2019-11-08 03:46:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,15 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
   
-  
+   def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
   
 end

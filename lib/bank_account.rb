dfef4f4da735169054d07b1a0c0150ac5178bd53
Gina Graci <ginagraci92@gmail.com>
2019-11-08 03:24:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
   
   def initialize(account_name)
     @name = account_name
     @balance = 1000
+    @status = "open"
   end 
 
 end

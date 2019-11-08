d4ac43fbf28df295d9f7c97eda5b66c0078e0c05
Gina Graci <ginagraci92@gmail.com>
2019-11-08 03:22:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :balance
   
-  def initialize(account_name)
+  def initialize(account_name, balance = "1000")
     @name = account_name
+    @balance = balance
   end 
 
 end
